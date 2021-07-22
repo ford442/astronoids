28170de1b74dd1e2d5072222c218a9824dcc65e7
Noah Cohn <noahc42@gmail.com>
2021-07-22 02:37:00 -0700

    Update mixer.h

--- src/mixer.h
+++ src/mixer.h
@@ -7,7 +7,7 @@
 bool mixer_init(int num_mixer_channels);
 void mixer_set_channel_completion_handler(void (*callback)(int channel));
 int mixer_load_sample_from_file(const char *path);
-int mixer_play_sample(int sample);
+int mixer_play_sample(register int sample);
 bool mixer_stop_playing_on_channel(int channel);
 void mixer_cleanup();
 #endif
