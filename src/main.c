02dbc02382e238cc3eb75878976f00ee6eb1e58b
Noah Cohn <noahc42@gmail.com>
2021-08-06 02:40:43 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,8 +28,6 @@ int main(int argc,char **argv){
 bool fullscreen=false;
 bool sandbox=false;
 bool silent=false;
-
-debug_init();
 if(argc > 1){
 for (int i=1; i < argc; ++i){
 if(strcmp(argv[i],FULLSCREEN_FLAG) == 0){
@@ -50,9 +48,6 @@ SDL_Init(0);
 #ifdef __EMSCRIPTEN__
 SDL_SetHint(SDL_HINT_NO_SIGNAL_HANDLERS, "1");
 #endif
-if(false == video_init(LOGICAL_WIDTH_PX,LOGICAL_HEIGHT_PX,"Asteroids",fullscreen)){
-exit(1);
-}
 if(!silent){
 if(false == mixer_init(MIXER_DEFAULT)){
 exit(1);
