a3b150275707fdeafff4e4443694469b8860074b
Noah Wolfe Cohn <noahc42@gmail.com>
2021-08-06 02:54:59 -0700

    edit

--- src/main.c
+++ src/main.c
@@ -19,12 +19,8 @@ void help(void){
 
 int main(int argc,char **argv){
 }
-}
-atexit(cleanup);
-SDL_Init(0);
-#ifdef __EMSCRIPTEN__
+
 SDL_SetHint(SDL_HINT_NO_SIGNAL_HANDLERS, "1");
-#endif
 if(!silent){
 }
 return 0;
